2d6424ec71bc405760c52ad2d1097ab5cb87cefa
Edi6758 <ediarts7@gmail.com>
2023-01-05 19:28:05 -0300

    :sparkles: arrow function
    arrow function

---
+++ arrow function/script.js
@@ -0,0 +1,15 @@
+const multiply = (a, b) => a * b
+
+function normalSum(a, b) {
+    return a+b
+}
+
+const anonymusSum = function (a, b) {
+    return a+b
+}
+
+const arrowSum = (a, b) => {
+    return a+b
+}
+
+const arrowSimpleSum = (a, b) => a * b
