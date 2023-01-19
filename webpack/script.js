ef9d51c5a7ef6f24f27bf6a90851f65e48b57304
Edi6758 <ediarts7@gmail.com>
2023-01-18 23:39:02 -0300

    :construction: how to use webpack
    how to use webpack

--- webpack/script.js
+++ webpack/script.js
@@ -1,2 +1,15 @@
 //o webpaco pega uma série de arquivos, modulos e etc e empacota em um único arquivo
 //voce pode executar o babel junto com o webpack para empacotar tudo.
+//o webpack empacota tudo em um único arquivo compilado removendo a necessidade de ter a node modules
+
+//utilizando webpack
+
+//npm init -y
+//npm install --save-dev webpack webpack-cli
+//npm install --save dayjs
+
+import dayjs from 'dayjs'
+
+alert(`Hoje é: ${dayjs().format("DD/MM/YYYY")}`)
+
+//npx webpack
