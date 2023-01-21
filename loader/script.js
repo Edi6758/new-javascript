095c4db0687820c6fb87fd176e42331cf420e75d
Edi6758 <ediarts7@gmail.com>
2023-01-21 13:59:05 -0300

    :construction: completed script loadres in webpack
    completed script loadres in webpack

--- loader/script.js
+++ loader/script.js
@@ -1,11 +1,15 @@
 //sistema de empacotamento do webpack
 
 
-const path = require('path')
-
 module.exports = {
     entry: {
         index: './src/index.js'
     },
-    mode: 'development'
+    mode: 'development',
+    module: {
+        rules: [{
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader']
+        }]
+    }
 }
