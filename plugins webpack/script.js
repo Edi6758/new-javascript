0619ed399c8b38f8571ad4076a23dfd5484e86c0
Edi6758 <ediarts7@gmail.com>
2023-01-22 19:58:33 -0300

    :sparkles: finish studys from plugin in webpack
    finish studys from plugin in webpack

---
+++ plugins webpack/script.js
@@ -0,0 +1,23 @@
+//npm install --save-dev mini-css-extract-plugin
+
+const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = {
+    entry: {
+        index: './src/index.js'
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist')
+    },
+    mode: 'development',
+    module: {
+        rules: [{
+            test: /\.css$/,
+            use: [MiniCssExtractPlugin.loader, 'css-loader']
+        }]
+    },
+    plugins: [
+        new MiniCssExtractPlugin()
+    ]
+}
